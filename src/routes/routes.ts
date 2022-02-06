943c83bfd260a857da14719298cc5b49262fdf3e
isntma <isntma@tuta.io>
2022-02-06 01:32:00 +0100

    first commit

---
+++ src/routes/routes.ts
@@ -0,0 +1,204 @@
+import { Request, Response, Router } from 'express'
+import { db } from '../database/database'
+import { Cliente } from "../classes/personal/cliente";
+import { Empleado } from "../classes/personal/empleado";
+import { Producto } from "../classes/producto/producto";
+import { Dispositivo } from "../classes/producto/dispositivo";
+import { Liquido } from "../classes/producto/liquido";
+import { tCliente, ClienteDB } from "../schemas/cliente";
+import { tEmpleado, EmpleadoDB } from "../schemas/empleado";
+import { tLiquido, LiquidoDB } from "../schemas/liquido";
+import { tDispositivo, DispositivoDB } from "../schemas/dispositivo";
+import { tCompra, CompraDB } from "../schemas/compras";
+import { Mongoose } from 'mongoose';
+import { CallTracker } from 'assert';
+import { publicEncrypt } from 'crypto';
+
+class Routes {
+  private _router: Router
+
+  constructor() {
+    this._router = Router()
+  }
+  get router() {
+    return this._router
+  }
+
+  // Listar empleados
+  private listEmpleados = async (req: Request, res: Response) => {
+    await db.conectarBD()
+      .then(async (mensaje) => {
+        const valor = req.params.id
+        console.log(mensaje)
+        const query = await EmpleadoDB.find();
+        res.json(query)
+      })
+      .catch((mensaje) => {
+        res.send(mensaje)
+      })
+    db.desconectarBD()
+  }
+
+  // Listar clientes
+  private listClientes = async (req: Request, res: Response) => {
+    await db.conectarBD()
+      .then(async (mensaje) => {
+        const valor = req.params.id
+        console.log(mensaje)
+        const query = await ClienteDB.find();
+        res.json(query)
+      })
+      .catch((mensaje) => {
+        res.send(mensaje)
+      })
+    db.desconectarBD()
+  }
+
+  // Añadir empleado
+  private addEmpleado = async (req: Request, res: Response) => {
+    const { id, nombre, direccion, telefono, email, password, fechanacimiento, puesto, socio } = req.body
+    await db.conectarBD()
+    const dSchema = {
+      _id: id,
+      _nombre: nombre,
+      _direccion: direccion,
+      _telefono: telefono,
+      _email: email,
+      _password: password,
+      _fechanacimiento: fechanacimiento,
+      _puesto: puesto,
+      _socio: socio,
+    }
+    const oSchema = new EmpleadoDB(dSchema)
+    await oSchema.save()
+      .then((doc: any) => res.send('Has guardado el archivo:\n' + doc))
+      .catch((err: any) => res.send('Error: ' + err))
+
+    db.desconectarBD()
+  }
+
+  // Añadir cliente
+  private addCliente = async (req: Request, res: Response) => {
+    const { id, nombre, direccion, telefono, email, password, fechanacimiento, socio } = req.body
+    await db.conectarBD()
+    const dSchema = {
+      _id: id,
+      _nombre: nombre,
+      _direccion: direccion,
+      _telefono: telefono,
+      _email: email,
+      _password: password,
+      _fechanacimiento: fechanacimiento,
+      _socio: socio,
+      _carrito: [],
+    }
+    const oSchema = new ClienteDB(dSchema)
+    await oSchema.save()
+      .then((doc: any) => res.send('Has guardado el archivo:\n' + doc))
+      .catch((err: any) => res.send('Error: ' + err))
+
+    db.desconectarBD()
+  }
+
+  // Actualizar empleado
+  private updateEmpleado = async (req: Request, res: Response) => {
+    await db.conectarBD()
+    const id = req.params.id
+    const { nombre, direccion, telefono, email, password, fechanacimiento, puesto, socio } = req.body
+    await EmpleadoDB.findOneAndUpdate(
+        { _id: id },
+        {
+          _id: id,
+          _nombre: nombre,
+          _direccion: direccion,
+          _telefono: telefono,
+          _email: email,
+          _password: password,
+          _fechanacimiento: fechanacimiento,
+          _puesto: puesto,
+          _socio: socio,
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+        .then((doc: any) => res.send('Has guardado el archivo:\n' + doc))
+        .catch((err: any) => res.send('Error: ' + err))
+
+    await db.desconectarBD()
+}
+
+  // Actualizar cliente
+  private updateCliente = async (req: Request, res: Response) => {
+    await db.conectarBD()
+    const id = req.params.id
+    const { nombre, direccion, telefono, email, password, fechanacimiento, socio } = req.body
+    await ClienteDB.findOneAndUpdate(
+        { _id: id },
+        {
+          _id: id,
+          _nombre: nombre,
+          _direccion: direccion,
+          _telefono: telefono,
+          _email: email,
+          _password: password,
+          _fechanacimiento: fechanacimiento,
+          _socio: socio,
+          _carrito: [],
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+        .then((doc: any) => res.send('Has guardado el archivo:\n' + doc))
+        .catch((err: any) => res.send('Error: ' + err))
+
+    await db.desconectarBD()
+}
+
+  // Eliminar empleado
+  private delEmpleado = async (req: Request, res: Response) => {
+    await db.conectarBD()
+
+    const id = req.params.id
+    await EmpleadoDB.findOneAndDelete({ _id: id })
+    .then((doc: any) => res.send('Eliminado correctamente.'))
+    .catch((err: any) => res.send('Error: ' + err))
+
+    await db.desconectarBD()
+}
+
+  // Eliminar cliente
+  private delCliente = async (req: Request, res: Response) => {
+    await db.conectarBD()
+
+    const id = req.params.id
+    await ClienteDB.findOneAndDelete({ _id: id })
+    .then((doc: any) => res.send('Eliminado correctamente.'))
+    .catch((err: any) => res.send('Error: ' + err))
+
+    await db.desconectarBD()
+}
+
+// RUTAS
+  misRutas() {
+    // Listar
+    this._router.get('/empleados', this.listEmpleados)
+    this._router.get('/clientes', this.listClientes)
+    // Crear
+    this._router.post('/empleados/addEmpleado', this.addEmpleado)
+    this._router.post('/clientes/addCliente', this.addCliente)
+    // Actualizar
+    this._router.put('/empleados/update/:id', this.updateEmpleado)
+    this._router.put('/clientes/update/:id', this.updateCliente)
+    // Eliminar
+    this._router.delete('/empleados/delete/:id', this.delEmpleado)
+    this._router.delete('/clientes/delete/:id', this.delCliente)
+  }
+}
+
+const obj = new Routes()
+obj.misRutas()
+export const routes = obj.router
---
+++ src/schemas/empleado.ts
@@ -0,0 +1,55 @@
+import { Schema, model } from "mongoose";
+
+const empleadoSchema = new Schema({
+  _id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  _nombre: {
+    type: String,
+  },
+  _direccion: {
+    type: {
+      calle: String,
+      numero: Number,
+    },
+  },
+  _telefono: {
+    type: Number,
+  },
+  _email: {
+    type: String,
+  },
+  _password: {
+    type: String,
+    required: true
+  },
+  _fechanacimiento: {
+    type: Date,
+  },
+  _puesto: {
+    type: String,
+  },
+  _socio: {
+    type: Boolean,
+  },
+});
+
+export type tEmpleado = {
+  //exporta tipo empleado
+  _id: string | null;
+  _nombre: string | null;
+  _direccion: {
+    calle: string;
+    numero: number;
+  } | null;
+  _telefono: number | null;
+  _email: string | null;
+  _password: string | null;
+  _fechanacimiento: Date | null;
+  _puesto: string | null;
+  _socio: Boolean | null;
+};
+
+export const EmpleadoDB = model("empleados", empleadoSchema);
